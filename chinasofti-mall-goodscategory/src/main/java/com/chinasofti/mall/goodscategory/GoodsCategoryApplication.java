01f303fd7a7441d6a3474a850f5bb7c6a7c11686
649959626 <649959626@qq.com>
2017-11-01 21:41:38 +0800

    商品分类服务

--- chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodscategory/GoodsCategoryApplication.java
+++ chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodscategory/GoodsCategoryApplication.java
@@ -1,8 +1,17 @@
 package com.chinasofti.mall.goodscategory;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.chinasofti.mall.goodscategory.mapper")
 public class GoodsCategoryApplication {
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GoodsCategoryApplication.class, args);
+	}
+	
 }
