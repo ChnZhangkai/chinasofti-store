efe8f561114b21182ead34ffd7070670301ecb49
649959626 <649959626@qq.com>
2017-11-02 09:45:55 +0800

    修改商品分类

--- chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodscategory/controller/GoodsCategoryController.java
+++ chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodscategory/controller/GoodsCategoryController.java
@@ -8,24 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.chinasofti.mall.common.controller.BaseController;
 import com.chinasofti.mall.common.entity.Goodscategory;
 import com.chinasofti.mall.common.entity.GoodscategoryExample;
-import com.chinasofti.mall.common.service.IBaseService;
-import com.chinasofti.mall.goodscategory.service.GoodsCategoryService;
+import com.chinasofti.mall.goodscategory.service.impl.GoodsCategoryServiceImpl;
 
 @RestController
 @RequestMapping("/goods")
-public class GoodsCategoryController extends BaseController<Goodscategory>{
+public class GoodsCategoryController {
 	
 	@Autowired
-	private GoodsCategoryService goodsCategoryService;
-	
-	@Override
-	public IBaseService<Goodscategory> getService() {
-		
-		return goodsCategoryService;
-	}
+	private GoodsCategoryServiceImpl goodsCategoryService;
 	
 	@RequestMapping(value = "/select" , method = RequestMethod.POST)
 	public List<Goodscategory> selectByGoodsCategory(@RequestBody(required = false)Goodscategory goodscategory){
