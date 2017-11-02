9a5c1848bd73a15157df487b8409204b0d1b4530
xiaokunxiong <xiao_kun_xiong@163.com>
2017-11-02 10:31:05 +0800

    no changes

--- chinasofti-mall-web-entrance/src/main/java/com/chinasofti/mall/web/entrance/controller/GoodsController.java
+++ chinasofti-mall-web-entrance/src/main/java/com/chinasofti/mall/web/entrance/controller/GoodsController.java
@@ -3,11 +3,17 @@ package com.chinasofti.mall.web.entrance.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
+/**
+ * 
+* @ClassName: GoodsController
+* @Description: TODO(这里用一句话描述这个类的作用)
+* @author xiaokunxiong
+* @date 2017年11月1日 下午2:55:08 
+*
+ */
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
-	
 	@RequestMapping("/index")
 	public ModelAndView toIndex(){
 		return new ModelAndView("/goods/goods");
