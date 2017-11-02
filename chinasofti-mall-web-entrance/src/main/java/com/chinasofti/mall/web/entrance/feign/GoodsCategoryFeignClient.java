d63e895dd714efbd2abc39ee28d45d35cff9e86d
649959626 <649959626@qq.com>
2017-11-02 16:12:13 +0800

    修改

--- chinasofti-mall-web-entrance/src/main/java/com/chinasofti/mall/web/entrance/feign/GoodsCategoryFeignClient.java
+++ chinasofti-mall-web-entrance/src/main/java/com/chinasofti/mall/web/entrance/feign/GoodsCategoryFeignClient.java
@@ -1,6 +1,7 @@
 package com.chinasofti.mall.web.entrance.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +9,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.chinasofti.mall.common.entity.Goodscategory;
 import com.chinasofti.mall.web.entrance.hystrix.GoodsCategoryFeignClientHystrix;
 
+/**
+ * Feign服务调用+负载均衡
+ * @author Administrator
+ *
+ */
 @FeignClient(name = "goodscategoryService" , fallback = GoodsCategoryFeignClientHystrix.class)
 public interface GoodsCategoryFeignClient {
 	
+	/**
+	 * 列表及条件查询
+	 * @param goodscategory
+	 * @return
+	 */
 	@RequestMapping(value = "/goods/select" , method = RequestMethod.POST)
 	public String selectByGoodsCategory(@RequestBody(required = false) Goodscategory goodscategory);
+	
+	@RequestMapping(value = "/goods/delete/{ids}" , method =RequestMethod.POST)
+	public int deleteGoodsCategory(@PathVariable("ids") Integer ids);
+	
+	@RequestMapping(value = "/select/{ids}" , method = RequestMethod.POST)
+	public Goodscategory selectById(Integer ids);
 	
 }
