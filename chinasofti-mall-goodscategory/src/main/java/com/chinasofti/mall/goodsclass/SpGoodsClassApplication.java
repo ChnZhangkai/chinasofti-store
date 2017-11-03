83e105c3bfffad638f744c0ef4fe146256cc66af
649959626 <649959626@qq.com>
2017-11-03 12:47:16 +0800

    修改商品服务

--- chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodsclass/SpGoodsClassApplication.java
+++ chinasofti-mall-goodscategory/src/main/java/com/chinasofti/mall/goodsclass/SpGoodsClassApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@MapperScan("com.chinasofti.mall.goodscategory.mapper")
+@MapperScan("com.chinasofti.mall.goodsclass.mapper")
 public class SpGoodsClassApplication {
 	
 	public static void main(String[] args) {
