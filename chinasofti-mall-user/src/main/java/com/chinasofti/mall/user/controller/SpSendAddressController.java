740ff739c73f84e8701cedfcad5e0405345207e3
xiaozhiliang <33312709+xiaozhiliang@users.noreply.github.com>
2017-11-20 10:55:19 +0800

    优化地址接口

--- chinasofti-mall-user/src/main/java/com/chinasofti/mall/user/controller/SpSendAddressController.java
+++ chinasofti-mall-user/src/main/java/com/chinasofti/mall/user/controller/SpSendAddressController.java
@@ -36,8 +36,7 @@ public class SpSendAddressController {
 	SpSendAddressService spSendAddressService;
 	private static final Logger logger = LoggerFactory.getLogger(SpSendAddressController.class);
 	
-	private String retCode ;
-	private String retMsg ;
+	
 	
 	/*
 	 * 查询地址列表
@@ -46,10 +45,8 @@ public class SpSendAddressController {
 	@RequestMapping(value="/list")
 	public ResponseInfo findSendAddressList(@RequestParam("userId") String userId) {
 		ResponseInfo res = new ResponseInfo();
-		List<SpSendAddress> list = new ArrayList<SpSendAddress>();
 		try{
-		list = spSendAddressService.querySentAddressList(userId);
-		res = dealResponseData(list);
+			res = spSendAddressService.querySentAddressList(userId);
 		}catch(Exception e){
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
@@ -64,15 +61,8 @@ public class SpSendAddressController {
 	@RequestMapping(value="/add")
 	public ResponseInfo addSendAddress(@RequestBody SpSendAddress spSendAddress){
 		ResponseInfo res = new ResponseInfo();
-		//验证地址信息
-		if(!checkAddrInfo(spSendAddress)){
-			res.setRetCode(retCode);
-			res.setRetMsg(retMsg);
-			return res;
-		}
 		try{
-			res = dealResponseData(spSendAddressService.insertSendAddress(spSendAddress));
-			logger.info(res.toString());
+			res = spSendAddressService.insertSendAddress(spSendAddress);
 		}catch(Exception e){
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
@@ -88,13 +78,8 @@ public class SpSendAddressController {
 	@RequestMapping(value="/updata")
 	public ResponseInfo updataSendAddress(@RequestBody SpSendAddress spSendAddress){
 		ResponseInfo res = new ResponseInfo();
-		if(!"".equals(spSendAddress.getMobile())&&!checkAddrInfo(spSendAddress)){
-			res.setRetCode(retCode);
-			res.setRetMsg(retMsg);
-			return res;
-		}
 		try{
-			res = dealResponseData(spSendAddressService.updateSendAddress(spSendAddress));
+			res = spSendAddressService.updateSendAddress(spSendAddress);
 		}catch(Exception e){
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
@@ -110,80 +95,12 @@ public class SpSendAddressController {
 	public ResponseInfo deleteSendAddress(@RequestBody SpSendAddress spSendAddress){
 		ResponseInfo res = new ResponseInfo();
 		try{
-			res = dealResponseData(spSendAddressService.deleteSendAddress(spSendAddress));
+			res = spSendAddressService.deleteSendAddress(spSendAddress);
 		}catch(Exception e){
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
 		}
 		return res;
-	}
-	
-	//封装返回参数
-	private ResponseInfo dealResponseData(Object obj) {
-		ResponseInfo  response= new ResponseInfo();
-		if(obj !=null){
-			Map<String, Object> data= new HashMap<String, Object>();
-			data.put("responseInfo", obj);
-			response.setData(data);
-			response.setRetCode(MsgEnum.SUCCESS.getCode());
-			response.setRetMsg(MsgEnum.SUCCESS.getMsg());
-		}else{
-			response.setRetCode(MsgEnum.ERROR.getCode());
-			response.setRetMsg(MsgEnum.ERROR.getMsg());
-		}
-		
-		return response;
 	}	
 	
-	private boolean checkAddrInfo(SpSendAddress spSendAddress){
-		if(!checkMobile(spSendAddress)){
-			retCode = "600010";
-			retMsg = "请输入正确的手机号码";
-			return false;
-		}
-		if(!checkWard(spSendAddress)){
-			retCode = "600011";
-			retMsg = "请输入完整的省市区";
-			return false;
-		}
-		if(!checkAddress(spSendAddress)){
-			retCode = "600012";
-			retMsg = "请输入正确的地址";
-			return false;
-		}
-		return true;
-		
-	}
-	
-	//验证手机号码
-	private boolean checkMobile(SpSendAddress spSendAddress){ 
-		String mobile = spSendAddress.getMobile();
-		String regExp = "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";  
-        Pattern p = Pattern.compile(regExp);  
-        Matcher m = p.matcher(mobile);  
-        return m.matches();
-		
-	}
-	//验证省市区
-	private boolean checkWard(SpSendAddress spSendAddress){
-		String province=spSendAddress.getProvince();
-		String city=spSendAddress.getCity();
-		String district=spSendAddress.getDistrict();
-		String address=spSendAddress.getAddress();
-		if("".equals(province)||"".equals(city)||"".equals(district)||"".equals(address)){
-			return false;
-		}
-		return true;
-		
-	}
-	//验证地址
-	//验证省市区
-		private boolean checkAddress(SpSendAddress spSendAddress){
-			String address=spSendAddress.getAddress();
-			if("".equals(address)){
-				return false;
-			}
-			return true;
-			
-		}
 }
