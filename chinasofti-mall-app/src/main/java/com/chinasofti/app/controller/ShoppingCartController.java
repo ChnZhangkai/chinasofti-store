8bfedcea682efc7a55722dba9c4a606003b59531
Administrator <Administrator@WRGHO-609111810>
2017-11-20 14:17:33 +0800

    优化购物车相关操作如，增、删、改、查等功能

--- chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/ShoppingCartController.java
+++ chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/ShoppingCartController.java
@@ -3,17 +3,20 @@ package com.chinasofti.app.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alibaba.fastjson.JSONObject;
 import com.chinasofti.app.feign.ShoppingCartFeignClient;
+import com.chinasofti.mall.common.entity.order.PyShoppingCart;
 import com.chinasofti.mall.common.utils.ResponseInfo;
 
 /**
@@ -37,10 +40,10 @@ public class ShoppingCartController {
 	@ResponseBody
 	@RequestMapping(value="/del/goods", method = RequestMethod.POST)
 	@ApiOperation(value="删除购物车商品", notes="报文示例：{\"goodsList\":[{\"id\":\"1001\"},{\"id\":\"1002\"}]}")
-	public ResponseInfo deletePyShoppingCartById(@RequestBody JSONObject json,HttpServletResponse response) {
+	public ResponseInfo deletePyShoppingCartById(@RequestBody List<PyShoppingCart> goodsList,HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods","POST");
-		ResponseInfo responseInfo = shoppingCartFeignClient.deletePyShoppingCartById(json);
+		ResponseInfo responseInfo = shoppingCartFeignClient.deletePyShoppingCartById(goodsList);
 		return responseInfo;
 	}
 	
@@ -52,10 +55,10 @@ public class ShoppingCartController {
 	@ResponseBody
 	@RequestMapping(value="/add/goods", method = RequestMethod.POST)
 	@ApiOperation(value="添加购物车商品", notes="报文示例：{\"goodsList\":[{\"goodsId\":\"1001\",\"userId\":\"chin\",\"goodsNum\":\"1\"},{\"goodsId\":\"1002\",\"userId\":\"chin\",\"goodsNum\":\"2\"}]}")
-	public ResponseInfo savePyShoppingCart(@RequestBody JSONObject json,HttpServletResponse response) {
+	public ResponseInfo savePyShoppingCart(@RequestBody List<PyShoppingCart> goodsList,HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods","POST");
-		ResponseInfo responseInfo = shoppingCartFeignClient.savePyShoppingCart(json);
+		ResponseInfo responseInfo = shoppingCartFeignClient.savePyShoppingCart(goodsList);
 		return responseInfo;
 	}
 
@@ -67,10 +70,10 @@ public class ShoppingCartController {
 	@ResponseBody
 	@RequestMapping(value="/mod/goods", method = RequestMethod.POST)
 	@ApiOperation(value="修改购物车商品数量", notes="报文示例：{\"goodsList\":[{\"ids\":\"1\",\"goodsId\":\"1001\",\"userId\":\"chinasofti\",\"goodsNum\":\"3\"},{\"ids\":\"1\",\"goodsId\":\"1002\",\"userId\":\"chinasofti\",\"goodsNum\":\"3\"}]}")
-	public ResponseInfo updatePyShoppingCart(@RequestBody JSONObject json,HttpServletResponse response) {
+	public ResponseInfo updatePyShoppingCart(@RequestBody List<PyShoppingCart> goodsList,HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods","POST");
-		ResponseInfo responseInfo = shoppingCartFeignClient.updatePyShoppingCart(json);
+		ResponseInfo responseInfo = shoppingCartFeignClient.updatePyShoppingCart(goodsList);
 		return responseInfo;
 	}
 	
@@ -79,13 +82,12 @@ public class ShoppingCartController {
 	 * @param json
 	 * @return
 	 */
-	@ResponseBody
 	@RequestMapping(value="/query/goodsList", method = RequestMethod.POST)
 	@ApiOperation(value="查询购物车商品", notes="报文示例：{\"userId\":\"1\"}")
-	public ResponseInfo queryPyShoppingCartListByUserId(@RequestBody JSONObject json,HttpServletResponse response){
+	public ResponseInfo queryPyShoppingCartListByUserId(@RequestParam("userId") String userId,HttpServletResponse response){
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods","POST");
-		ResponseInfo responseInfo = shoppingCartFeignClient.queryPyShoppingCartListByUserId(json);
+		ResponseInfo responseInfo = shoppingCartFeignClient.queryPyShoppingCartListByUserId(userId);
 		return responseInfo;
 	}
 
