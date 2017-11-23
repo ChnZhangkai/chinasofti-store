bfb8c945b925ab76fea17569a1528c53afe16bdd
Administrator <Administrator@WRGHO-609111810>
2017-11-23 17:44:20 +0800

    封装前端需要购物车列表数据

--- chinasofti-mall-common/src/main/java/com/chinasofti/mall/common/utils/Constant.java
+++ chinasofti-mall-common/src/main/java/com/chinasofti/mall/common/utils/Constant.java
@@ -7,5 +7,7 @@ public class Constant {
 	public static final String PAY_STATUS = "0";
 	//商品选中状态1选中 0未选择
 	public static final String CHECKED = "0";
+	//商品图片URL
+	public static final String HOST_URL ="http://192.168.1.244";
 
 }
