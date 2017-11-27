d6e10dc48d54e23eaa5aaa1503880d34f63d82c9
Administrator <Administrator@WRGHO-609111810>
2017-11-27 10:38:08 +0800

    更改购物车选定状态的数据类型为boolean型

--- chinasofti-mall-common/src/main/java/com/chinasofti/mall/common/utils/Constant.java
+++ chinasofti-mall-common/src/main/java/com/chinasofti/mall/common/utils/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
 	//支付状态 0  未支付 1 已支付 2 取消 
 	public static final String PAY_STATUS = "0";
 	//商品选中状态1选中 0未选择
-	public static final String CHECKED = "0";
+	public static final Boolean CHECKED = false;
 	//商品图片URL
 	public static final String HOST_URL ="http://192.168.1.244";
 
