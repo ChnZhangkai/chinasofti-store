b2f2835f316bb7edbeaf756735ed6a6c760a9dd4
ye_dawang123@163.com <ydw**1992**>
2017-12-04 16:16:48 +0800

    提交

--- chinasofti-mall-batch/src/main/java/com/chinasofti/mall/batch/service/impl/CancelGoodsOrdersServiceImpl.java
+++ chinasofti-mall-batch/src/main/java/com/chinasofti/mall/batch/service/impl/CancelGoodsOrdersServiceImpl.java
@@ -16,9 +16,6 @@ public class CancelGoodsOrdersServiceImpl implements CancelGoodsOrdersService {
 	public void updatePaywayStatus(PyBigGoodsorder pyBigGodsOrder) throws Exception{
 		try{
 			this.pyBigGoodsorderMapper.updatePaywayStatus(pyBigGodsOrder);
-			if("0f112f31de8746e4af098077425259a0".equals(pyBigGodsOrder.getIds())){
-				throw new Exception();
-			}
 		}catch(Exception e){
 			e.printStackTrace();
 			throw new Exception("CancelGoodsOrdersService.updatePaywayStatus()更新支付状态失败!"); 
