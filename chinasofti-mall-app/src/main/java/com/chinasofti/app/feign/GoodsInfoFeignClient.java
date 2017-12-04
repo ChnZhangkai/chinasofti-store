282999dad925db57309871cf6c5085d72242852c
Administrator <Administrator@WRGHO-609111810>
2017-12-04 17:25:57 +0800

    商品详情优化

--- chinasofti-mall-app/src/main/java/com/chinasofti/app/feign/GoodsInfoFeignClient.java
+++ chinasofti-mall-app/src/main/java/com/chinasofti/app/feign/GoodsInfoFeignClient.java
@@ -1,15 +1,17 @@
 package com.chinasofti.app.feign;
 
 
+import java.util.List;
+
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.chinasofti.app.hystrix.GoodsFeignClientHystrix;
+import com.chinasofti.mall.common.entity.goods.ChnGoodsInfoVo;
 import com.chinasofti.mall.common.entity.goods.ChnGoodsinfo;
-import com.chinasofti.mall.common.utils.ResponseInfo;
- 
+
 /**分类消费类
  * 
  * @author gezhenlin
@@ -24,12 +26,12 @@ public interface GoodsInfoFeignClient {
 	 * @return
 	 */
 	@RequestMapping(value = "/goodsInfo/queryGoodsList")
-	public ResponseInfo queryGoodList(@RequestParam("id") String id);
+	public List<ChnGoodsinfo> queryGoodList(@RequestParam("id") String id);
   
 	@RequestMapping(value = "/goodsInfo/queryGoodsInfo")
-	public ResponseInfo queryGoodInfo(@RequestParam("ids") String ids);
+	public ChnGoodsInfoVo queryGoodInfo(@RequestParam("ids") String ids);
 	
 	@RequestMapping(value = "/goodsInfo/queryGoodsInfoList")
-	public ResponseInfo queryGoodsInfoList(@RequestBody ChnGoodsinfo goodsInfo);
+	public List<ChnGoodsinfo> queryGoodsInfoList(@RequestBody ChnGoodsinfo goodsInfo);
   
 }
--- chinasofti-mall-goods/src/main/java/com/chinasofti/mall/goods/service/IChnGoodsInfoService.java
+++ chinasofti-mall-goods/src/main/java/com/chinasofti/mall/goods/service/IChnGoodsInfoService.java
@@ -1,6 +1,9 @@
 package com.chinasofti.mall.goods.service;
 
 
+import java.util.List;
+
+import com.chinasofti.mall.common.entity.goods.ChnGoodsInfoVo;
 import com.chinasofti.mall.common.entity.goods.ChnGoodsinfo;
 import com.chinasofti.mall.common.utils.ResponseInfo;
 
@@ -11,14 +14,14 @@ public interface IChnGoodsInfoService{
      * @param goodsClassId
      * @return
      */
-   public ResponseInfo selectByClassId(String goodsClassIds);
+   public List<ChnGoodsinfo> selectByClassId(String goodsClassIds);
    
-   public ResponseInfo selectByGoodsId(String ids);
+   public ChnGoodsInfoVo selectByGoodsId(String ids);
    /**
     * 根据关键字title查询商品列表
     * @param record
     * @return
     */
-   public ResponseInfo selectByNameOrother(ChnGoodsinfo record);
+   public List<ChnGoodsinfo> selectByNameOrother(ChnGoodsinfo record);
 
 }
