3b464b64f685fb5a914a7ce3745016345fe95319
ye_dawang123@163.com <ydw**1992**>
2017-12-05 11:25:50 +0800

    批处理提交

--- chinasofti-mall-batch/src/main/java/com/chinasofti/mall/batch/service/CancelGoodsOrdersService.java
+++ chinasofti-mall-batch/src/main/java/com/chinasofti/mall/batch/service/CancelGoodsOrdersService.java
@@ -4,9 +4,10 @@ import com.chinasofti.mall.batch.bean.PyBigGoodsorder;
 
 public interface CancelGoodsOrdersService {
 	/**
-	 * 更新订单的支付状态
+	 * 更新大订单的支付状态
 	 * @param pyBigGodsOrder
 	 * @throws Exception
 	 */
 	void updatePaywayStatus(PyBigGoodsorder pyBigGodsOrder) throws Exception ;
+	
 }
