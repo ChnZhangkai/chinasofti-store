223c9aeb7ddc33b11bf3ec3ef404718a7ce401b1
xiaozhiliang <33312709+xiaozhiliang@users.noreply.github.com>
2017-12-07 15:24:27 +0800

    合并代码

--- chinasofti-mall-user/src/main/java/com/chinasofti/mall/user/controller/SpUserController.java
+++ chinasofti-mall-user/src/main/java/com/chinasofti/mall/user/controller/SpUserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.chinasofti.mall.common.entity.AdvertiseContents;
 import com.chinasofti.mall.common.entity.spuser.SpMerchantUser;
 import com.chinasofti.mall.common.entity.spuser.SpUser;
+import com.chinasofti.mall.common.utils.Aes;
 import com.chinasofti.mall.common.utils.MsgEnum;
 import com.chinasofti.mall.common.utils.ResponseInfo;
 import com.chinasofti.mall.user.handler.MyException;
@@ -36,12 +38,19 @@ public class SpUserController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(SpUserController.class);  
 	
+	@Value("${decryptKey}")
+	private String decryptKey;
+	
 	@RequestMapping(value="/signUp")
 	public ResponseInfo signUp(@RequestBody SpUser spUser) {
 		ResponseInfo res = new ResponseInfo();
 		try {
+			String password = Aes.aesDecrypt(spUser.getPassword(), decryptKey);//对该密码进行Aes解密
+			logger.info("key:"+decryptKey);
+			logger.info("解密后的ASE密码:"+password);
+			spUser.setPassword(password);
 			res = spUserService.add(spUser);
-		} catch (MyException e1) {
+		} catch (Exception e1) {
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
 		}
@@ -53,8 +62,11 @@ public class SpUserController {
 	public ResponseInfo signIn(@RequestBody SpUser spUser) {
 		ResponseInfo res = new ResponseInfo();
 		try {
+			String password = Aes.aesDecrypt(spUser.getPassword(), decryptKey);//对该密码进行Aes解密
+			logger.info("解密后的ASE密码:"+password);
+			spUser.setPassword(password);
 			res = spUserService.select(spUser);
-		} catch (MyException e1) {
+		} catch (Exception e1) {
 			res.setRetCode("999999");
 			res.setRetMsg("系统异常");
 		}
