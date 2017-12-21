79470124ee95ae2de9af02e8b062c330ebde08f0
649959626 <649959626@qq.com>
2017-12-21 20:20:03 +0800

    修改参数管理为异步加载

--- chinasofti-mall-web/src/main/java/com/chinasofti/mall/web/entrance/hystrix/ParamFeignClientHystrix.java
+++ chinasofti-mall-web/src/main/java/com/chinasofti/mall/web/entrance/hystrix/ParamFeignClientHystrix.java
@@ -1,6 +1,5 @@
 package com.chinasofti.mall.web.entrance.hystrix;
 
-import com.chinasofti.mall.common.entity.param.Param;
 import com.chinasofti.mall.web.entrance.feign.ParamFeigClient;
 
 import net.sf.json.JSONObject;
@@ -8,9 +7,11 @@ import net.sf.json.JSONObject;
 public class ParamFeignClientHystrix implements ParamFeigClient{
 
 	@Override
-	public JSONObject findAll(Param param) {
+	public JSONObject selectByPid(Integer pid) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+
 
 }
