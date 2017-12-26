204c2c01d27e48c00ad356883a9817477c22e94a
649959626 <649959626@qq.com>
2017-12-26 15:39:38 +0800

    修改APP专区jar包

--- chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/PrefectureController.java
+++ chinasofti-mall-app/src/main/java/com/chinasofti/app/controller/PrefectureController.java
@@ -2,7 +2,7 @@ package com.chinasofti.app.controller;
 
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
